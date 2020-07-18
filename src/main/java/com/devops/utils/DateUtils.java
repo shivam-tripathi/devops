285218d2cdd987b098c5b4993b2d1a576d6a2fca
Shivam Tripathi <shivamtripathi0108@gmail.com>
2020-07-19 04:24:48 +0530

    feat(utils/DateUtils): Add utils for date manipulation

---
+++ src/main/java/com/devops/utils/DateUtils.java
@@ -0,0 +1,19 @@
+package com.devops.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateUtils {
+    public static Date addDaysToDate(Date date, int days) {
+        GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		calendar.add(Calendar.DATE, days);
+		return calendar.getTime();
+    }
+
+    public static String getDateInFormat(Date date, String format) {
+        return new SimpleDateFormat(format).format(date);
+    }
+}
