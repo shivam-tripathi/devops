2aaa6a5b85e73301310ada60b9311e18688d971e
Shivam Tripathi <shivamtripathi0108@gmail.com>
2020-07-19 01:35:02 +0530

    feat(Config): Add config monostate

---
+++ src/main/java/com/devops/Config.java
@@ -0,0 +1,40 @@
+package com.devops;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Config {
+    private String env;
+    private Map<String, String> subnets;
+    private Map<String, String> securityGroups;
+
+    private static Config config = null;
+
+    static {
+        try {
+            Map<String, String> env = System.getenv();
+            ObjectMapper mapper = new ObjectMapper();
+            config = mapper.readValue(env.get("JAVA_CONFIG"), Config.class);
+        } catch (Exception e) {
+            System.out.println("Error Occurred" + e.getMessage() + e);
+            System.exit(1);
+        }
+    }
+
+    public String getEnv() {
+        return config.env;
+    }
+
+    public Map<String, String> getSubnets() {
+        return config.subnets;
+    }
+
+    public Map<String, String> getSecurityGroups() {
+        return config.securityGroups;
+    }
+
+    public static Config getConfig() {
+        return config;
+    }
+}
