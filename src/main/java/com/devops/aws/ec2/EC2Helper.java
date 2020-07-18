a0a13b204cabab3ab44e4823035792ae6ba7499c
Shivam Tripathi <shivamtripathi0108@gmail.com>
2020-07-19 01:45:18 +0530

    feat(ec2/addTagsToAttachedVolume): Add method to attach tags to a volume already attached to an instance

--- src/main/java/com/devops/aws/ec2/EC2Helper.java
+++ src/main/java/com/devops/aws/ec2/EC2Helper.java
@@ -1,7 +1,16 @@
 package com.devops.aws.ec2;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.Filter;
+import com.amazonaws.services.ec2.model.InstanceBlockDeviceMapping;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.Tag;
 
 public class EC2Helper {
     private static AmazonEC2 ec2Client = null;
@@ -10,5 +19,25 @@ public class EC2Helper {
             ec2Client = AmazonEC2ClientBuilder.standard().build();
         }
         return ec2Client;
+    }
+
+    public void addTagsToAttachedVolume(String instanceTagName, Tag[] tags) {
+        Reservation result = ec2Client.describeInstances(
+            new DescribeInstancesRequest()
+                .withFilters(new Filter().withName("tag:Name").withValues(instanceTagName))
+        ).getReservations().get(0);
+
+        List<InstanceBlockDeviceMapping> mappings = result.getInstances().get(0).getBlockDeviceMappings();
+        List<String> volumeIds = mappings
+            .stream()
+            .map(mapping -> mapping.getEbs().getVolumeId())
+            .collect(Collectors.toList());
+
+        ec2Client.createTags(
+            new CreateTagsRequest()
+                .withResources(volumeIds)
+                .withTags(tags)
+        );
+        System.out.println();
     }
 }
