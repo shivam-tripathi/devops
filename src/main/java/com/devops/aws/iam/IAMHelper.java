63cc8c82d1bf5a85c94409cbba18edd7f4a7c270
Shivam Tripathi <shivamtripathi0108@gmail.com>
2020-07-19 01:40:59 +0530

    feat(iam/instanceProfile): Add function to add instance profile

--- src/main/java/com/devops/aws/iam/IAMHelper.java
+++ src/main/java/com/devops/aws/iam/IAMHelper.java
@@ -2,6 +2,8 @@ package com.devops.aws.iam;
 
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.AddRoleToInstanceProfileRequest;
+import com.amazonaws.services.identitymanagement.model.CreateInstanceProfileRequest;
 
 public class IAMHelper {
     private static AmazonIdentityManagement iamClient = null;
@@ -13,5 +15,21 @@ public class IAMHelper {
                 .build();
         }
         return iamClient;
+    }
+
+    public void createInstanceProfile(String profileName, String ...roleNames) {
+        AmazonIdentityManagement iamClient = IAMHelper.getClient();
+        iamClient.createInstanceProfile(
+            new CreateInstanceProfileRequest()
+                .withInstanceProfileName(profileName)
+        ).getInstanceProfile();
+
+        for (String roleName: roleNames) {
+            iamClient.addRoleToInstanceProfile(
+                new AddRoleToInstanceProfileRequest()
+                    .withInstanceProfileName(profileName)
+                    .withRoleName(roleName)
+            );
+        }
     }
 }
