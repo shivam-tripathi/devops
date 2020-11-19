d2a5dc50a9abf5dd6db4f2d6404e0da042c9a86c
Shivam Tripathi <shivamtripathi@Shivams-MacBook-Pro.local>
2020-11-19 20:43:46 +0530

    feat(services/ECSActions): Validate tasks age

--- src/main/java/com/devops/aws/ecs/ECSService.java
+++ src/main/java/com/devops/aws/ecs/ECSService.java
@@ -1,8 +1,13 @@
 package com.devops.aws.ecs;
 
+import com.amazonaws.services.ecs.model.ListTasksRequest;
+import com.amazonaws.services.ecs.model.ListTasksResult;
 import com.amazonaws.services.ecs.model.UpdateServiceRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -15,6 +20,20 @@ public class ECSService {
                 new UpdateServiceRequest().withService(arn).withCluster(clusterArn).withForceNewDeployment(true)
         );
         System.out.println("Force new deployment for service: " + this);
+    }
+
+    public List<String> listAllTaskArns() {
+        String token = null;
+        List<String> res = new ArrayList<>();
+        do {
+            ListTasksResult iterRes = ECSHelper.getClient().listTasks(
+                new ListTasksRequest().withNextToken(token).withServiceName(arn).withCluster(clusterArn)
+            );
+            token = iterRes.getNextToken();
+            res.addAll(iterRes.getTaskArns());
+        } while (token != null);
+
+        return res;
     }
 
     @Override
--- src/main/java/com/devops/utils/DateUtils.java
+++ src/main/java/com/devops/utils/DateUtils.java
@@ -8,9 +8,9 @@ import java.util.GregorianCalendar;
 public class DateUtils {
     public static Date addDaysToDate(Date date, int days) {
         GregorianCalendar calendar = new GregorianCalendar();
-		calendar.setTime(date);
-		calendar.add(Calendar.DATE, days);
-		return calendar.getTime();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, days);
+        return calendar.getTime();
     }
 
     public static String getDateInFormat(Date date, String format) {
